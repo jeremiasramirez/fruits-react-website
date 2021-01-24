0433dd92e4196059ae21d34b1bbadbc1755733f4
Jeremias ramirez <jereramirez499@gmail.com>
2021-01-24 18:14:59 -0400

    creado listas de menu

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -7,7 +7,10 @@ const Navbar = ()=>{
     return <section className="navbar">
 
         <article className="menu__bar">
-
+            <li className="selected">Home</li>
+            <li>About US</li>
+            <li>Services</li>
+            <li>Cart</li>
         </article>
 
     </section>
