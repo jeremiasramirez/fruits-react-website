90607badd6801c2ee9170d22213be6c350236aa5
Jeremias ramirez <jereramirez499@gmail.com>
2021-01-24 20:07:36 -0400

    agregado navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import './navbar.component.css';
+ 
 
+const SearchBar = ()=>{
+    return  <article className="menu__searchbar animate">
+            <input  type="search" placeholder="Search here"></input>
+            
+        </article>
+}
 
 const Navbar = ()=>{
     
     return <section className="navbar">
 
-        <article className="menu__bar animateScale">
+        <article className="menu__bar animate">
             <li className="selected">Home</li>
             <li>About US</li>
             <li>Services</li>
             <li>Cart</li>
         </article>
 
+
+        <SearchBar />
     </section>
 }
 
