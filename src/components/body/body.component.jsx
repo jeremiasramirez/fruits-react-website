12dcf5acbc7e03b20e4147f427b6e7d4f2fd96fa
Jeremias ramirez <jereramirez499@gmail.com>
2021-01-24 20:48:21 -0400

    agregado descripcion + images

--- src/components/body/body.component.jsx
+++ src/components/body/body.component.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import './body.component.css';
+import logo from '../../images/orange-fruit.svg';
+
+const DescriptionBody = ()=>{
+    return <>
+        <p className="description">
+            Orange is not just a fruit, its more. Get to
+            know the multiple health benefits of adding oranges
+            in your daily diet.
+        </p>
+        <p className="read">Read more...</p>
+    </>
+}
+
+
+
+const InfoComponent = ()=>{
+    return <article className="item animate">
+        <h1 className="info__title"> Eat Fresh </h1>
+        <h2 className="info__subtitle"> Stay Healthy! </h2>
+        {
+            DescriptionBody()
+        }
+    </article>
+}
+
+
+
+const ImageComponent = ()=>{
+    return <article className="animate imageBody">
+          <img src={logo} alt="image" ></img>
+    </article>
+}
+
+
+
 
 const BodyComponent = ()=>{
     return <section className="body__container">
+
+        {
+            InfoComponent()
+        }
+        {
+            ImageComponent()
+        }
+
 
     </section>
 }
 
 export default BodyComponent;
+
+
+
+
--- src/pages/main/App.js
+++ src/pages/main/App.js
@@ -1,10 +1,12 @@
 import Navbar from '../../components/navbar/navbar.component';
+import BodyComponent from '../../components/body/body.component';
 import './App.css';
 
 const MainApp =()=> {
   return <>
     <Navbar />
 
+    <BodyComponent />
   </>
 }
 
