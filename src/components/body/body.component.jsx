00e4fa6a9008030acafca558c3308bd293ceed57
Jeremias ramirez <jereramirez499@gmail.com>
2021-01-24 20:10:37 -0400

    agregado de modulo mbody

---
+++ src/components/body/body.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './body.component.css';
+
+const BodyComponent = ()=>{
+    return <section className="body__container">
+
+    </section>
+}
+
+export default BodyComponent;
