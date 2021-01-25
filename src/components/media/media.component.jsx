538020b3740e173fd6266f3d5aa2c384dc07bcea
Jeremias ramirez <jereramirez499@gmail.com>
2021-01-24 22:15:02 -0400

    agregado de bottombar

--- src/components/media/media.component.jsx
+++ src/components/media/media.component.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import './media.component.css';
 
-
+import facebook from '../../images/social-media/facebook.svg'
+import twitter from '../../images/social-media/twitter.svg'
+import instagram from '../../images/social-media/instagram.svg'
+ 
+ 
 const SocialMediaComponent = ()=>{
 
-    return <section>
-        <h1>hola</h1>
+    return <section className='social__media'>
+        <article className="avatar-social animateScale"> 
+            <img src={facebook} alt="facebook" />
+        </article>
 
+        <article className="avatar-social animateScale"> 
+            <img src={twitter} alt="twitter" />
+        </article>
+        
+        <article className="avatar-social animateScale"> 
+            <img src={instagram} alt="instagram" />
+        </article>
+ 
     </section>
 
 }
